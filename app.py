d8581f038bbf9d9e82638f7fad113942b55d2a1d
Xiuming Chen <cc@cxm.cc>
2018-04-10 17:20:13 -0700

    Echo some more infomation.
    * method
    * replicated CURL command.
    
    An example of the new output
    ```
    === path ===
    /
    === method===
    GET
    === remote_address ===
    127.0.0.1
    === headers ===
    Cookie: cookiename=cookievalue
    User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML,
    like Gecko) Chrome/65.0.3325.181 Safari/537.36
    Connection: keep-alive
    Dnt: 1
    Host: localhost:8080
    Upgrade-Insecure-Requests: 1
    Cache-Control: max-age=0
    Accept:
    text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
    Accept-Language: en-US,en;q=0.9
    Accept-Encoding: gzip, deflate, br
    === cookies ===
    cookiename: cookievalue
    === data ===
    
    === curl ===
    curl http://localhost:8080/ --header "Cookie: cookiename=cookievalue"
    --header "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36
    (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36" --header
    "Connection: keep-alive" --header "Dnt: 1" --header "Host:
    localhost:8080" --header "Upgrade-Insecure-Requests: 1" --header
    "Cache-Control: max-age=0" --header "Accept:
    text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
    --header "Accept-Language: en-US,en;q=0.9" --header "Accept-Encoding:
    gzip, deflate, br"
    ```

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, request, Response
 app = Flask(__name__)
 
+def gen_curl_command(path):
+    cmd = ['curl']
+    if request.method.lower() != 'get':
+        cmd += ['--request', request.method]
+    cmd += ['http://{}/{}'.format(request.headers.get('Host'), path)]
+    for k, v in request.headers:
+        cmd.append('--header')
+        cmd.append('"{}: {}"'.format(k, v))
+    return ' '.join(cmd)
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def inspect(path):
     txt = ''
     txt += '=== path ===\n'
     txt += '/{}\n'.format(path)
+    txt += '=== method===\n'
+    txt += request.method + '\n'
     txt += '=== remote_address ===\n'
     txt += '{}\n'.format(request.remote_addr)
     txt += '=== headers ===\n'
@@ -17,6 +29,8 @@ def inspect(path):
         txt += '{}: {}\n'.format(k, v)
     txt += '=== data ===\n'
     txt += '{}\n'.format(request.data)
+    txt += '=== curl ===\n'
+    txt += gen_curl_command(path)
     headers = {'Server': 'github.com/cxmcc/webinspect'}
     return Response(txt, headers=headers, mimetype='text/plain')
 
